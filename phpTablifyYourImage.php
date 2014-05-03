85dd5df10099cdb9fed5342dde017cc0b72e039e
Asier Iturralde Sarasola <asier.iturralde@gmail.com>
2014-05-03 16:47:30 +0200

    Added one image to test and got the rgb value of one pixel

--- phpTablifyYourImage.php
+++ phpTablifyYourImage.php
@@ -1,6 +1,16 @@
 <?php
     function tablifyYourImage() {
-        echo "Hello!";
+        $im = imagecreatefromjpeg("iametza.jpg");
+        $rgb = imagecolorat($im, 10, 15);
+        $r = ($rgb >> 16) & 0xFF;
+        $g = ($rgb >> 8) & 0xFF;
+        $b = $rgb & 0xFF;
+        
+        var_dump($im);
+        var_dump($rgb);
+        var_dump($r);
+        var_dump($g);
+        var_dump($b);
     }
     
     tablifyYourImage();
