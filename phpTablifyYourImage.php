51353f6f2504ae5297eebc43f56004d6f146f732
Asier Iturralde Sarasola <asier.iturralde@gmail.com>
2014-05-03 17:15:34 +0200

    Get the first tablified jpg

--- phpTablifyYourImage.php
+++ phpTablifyYourImage.php
@@ -5,26 +5,44 @@
         list($width, $height, $image_type) = getimagesize($file);
         
         $im = imagecreatefromjpeg($file);
-        $rgb = imagecolorat($im, 10, 15);
-        $r = ($rgb >> 16) & 0xFF;
-        $g = ($rgb >> 8) & 0xFF;
-        $b = $rgb & 0xFF;
         
-        var_dump($width);
-        var_dump($height);
-        var_dump($image_type);
-        var_dump($im);
-        var_dump($rgb);
-        var_dump($r);
-        var_dump($g);
-        var_dump($b);
+        // Create the table.
+        $table = '<table cellspacing="0" cellpadding="0" border="0" style="margin-left: auto; margin-right: auto; font-size:0px;">';
         
-        for ($x = 0; $x < $width; $x++) {
-            for ($y = 0; $y < $height; $y++) {
-                var_dump(imagecolorat($im, $x, $y));
+        // Create the body of the table.
+        $table = $table . '<tbody>';
+        
+        for ($y = 0; $y < $height; $y++) {
+            
+            for ($x = 0; $x < $width; $x++) {
+                
+                // If it is the first column (0st), create an tr tag
+                if ($x == 0) {
+                    $table = $table . '<tr>';
+                }
+                
+                $rgb = imagecolorat($im, $x, $y);
+                $r = ($rgb >> 16) & 0xFF;
+                $g = ($rgb >> 8) & 0xFF;
+                $b = $rgb & 0xFF;
+                
+                $table = $table . '<td width="1" height="1" colspan="1" style="background-color: rgb(' . $r . ', ' . $g . ', ' . $b . ');">&nbsp;</td>';
+                
+                // If it is the last column, close the tr tag
+                if ($x == $width - 1) {
+                    $table = $table . '</tr>';
+                }
+                
+                // Add a new line
+                $table = $table;
             }
         }
+        
+        // Close the body of the table and the table itself
+        $table = $table . '</tbody></table>';
+        
+        return $table;
     }
     
-    tablifyYourImage("iametza.jpg");
+    echo tablifyYourImage("iametza.jpg");
 ?>
